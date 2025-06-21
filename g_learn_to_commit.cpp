3fc3c1eddcebf2e0a2a34976e348fe473941c09a
Swathi Ramesh <157286054+swathiramesh24@users.noreply.github.com>
2025-06-21 12:42:17 +0530

    Create g_learn_to_commit.cpp

---
+++ g_learn_to_commit.cpp
@@ -0,0 +1,16 @@
+/*
+Input: a = 5, b = 6, c = 15
+Output: 5 15
+Explanation: Only the value of a and c is printed as the line of code that prints b is commented.
+*/
+
+class Solution {
+  public:
+    void printValues(int a, int b, int c) {
+        // code here
+        
+        cout << a << " ";
+        //cout << b << " ";
+        cout << c << " ";
+    }
+};
