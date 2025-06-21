d549271313f5434a773beee08a810d61c0650bed
Swathi Ramesh <157286054+swathiramesh24@users.noreply.github.com>
2025-06-21 21:19:06 +0530

    Create g_ip_op.cpp

---
+++ g_ip_op.cpp
@@ -0,0 +1,17 @@
+/*
+Input:
+A = 1, B = 2
+Output:
+2
+Explanation:
+Multiplication of 1 and 2 is 2.
+*/
+
+//solution
+class Solution {
+  public:
+    int multiplication(int A, int B) {
+        return A*B;
+        
+    }
+};
