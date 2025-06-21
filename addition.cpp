018880928d92666c86ede3026efb1db65b0fa97c
Swathi Ramesh <157286054+swathiramesh24@users.noreply.github.com>
2025-06-21 21:03:20 +0530

    Create addition.cpp

---
+++ addition.cpp
@@ -0,0 +1,19 @@
+/*
+Input: a = 1, b = 2
+Output: 3
+Explanation: Addition of 1 and 2 is 3.
+
+Input: a = 10, b = 20
+Output: 30
+Explanation: Addition os 10 and 20 is 30.
+*/
+
+//answer
+
+class Solution {
+  public:
+    int addition(int a, int b) {
+        return a+b;
+        
+    }
+};
