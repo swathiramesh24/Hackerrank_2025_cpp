9f8afbc10a2d639ab2399041ff468aa1d73f00f1
Swathi Ramesh <157286054+swathiramesh24@users.noreply.github.com>
2025-06-24 19:03:33 +0530

    Create taking_input.cpp

---
+++ taking_input.cpp
@@ -0,0 +1,23 @@
+/*
+Input:
+a = 5
+b = Hello
+Output:
+5 Hello
+*/
+
+//solution
+
+class Solution {
+  public:
+    void inputData() {
+        
+        int a;
+        cin >> a;
+        
+        string b;
+        cin >> b;
+        
+        cout << a << " " << b << '\n';
+    }
+};
