d657bf7b3a02f757afc513a41ca56c3bf6442df4
Swathi Ramesh <157286054+swathiramesh24@users.noreply.github.com>
2025-06-26 18:10:07 +0530

    Create g_cpp_strings.cpp

---
+++ g_cpp_strings.cpp
@@ -0,0 +1,15 @@
+/*
+Given two strings  S1 and S2 . You have to concatenate both the strings and print the concatenated string.
+
+Example 1:
+Input:
+S1 = "Geeksfor"
+S2 = "Geeks"
+Output: GeeksforGeeks
+Explanation: Combined "Geeksfor" and "Geeks"
+*/
+
+//solution
+string conCat(string s1, string s2) {
+    return s1+s2;
+}
