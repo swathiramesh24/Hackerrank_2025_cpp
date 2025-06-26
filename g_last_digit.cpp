0481793c68281d949e6fba9f68cf89eda38bec5e
Swathi Ramesh <157286054+swathiramesh24@users.noreply.github.com>
2025-06-26 18:04:26 +0530

    Create g_last_digit.cpp

---
+++ g_last_digit.cpp
@@ -0,0 +1,16 @@
+/*
+Input: n = 10
+Output: 0
+Input: n = 9768
+Output: 8
+*/
+
+//solution
+void utility(int n) {
+
+    
+    int ans = abs(n%10); //abs to avoid negative numbers
+
+        
+        cout << ans << endl;
+}
