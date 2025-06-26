4f424bc57f45d114c5cfdf070ae342f541e9f668
Swathi Ramesh <157286054+swathiramesh24@users.noreply.github.com>
2025-06-26 18:38:43 +0530

    Create g_input_integer.cpp

---
+++ g_input_integer.cpp
@@ -0,0 +1,12 @@
+//Given a number n. Take input and add 10 to its value and print it.
+
+//solution
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cout << n+10;
+    return 0;
+}
