767b075484d969577013b1d238b24c62c14bba48
Swathi Ramesh <157286054+swathiramesh24@users.noreply.github.com>
2025-06-26 18:42:09 +0530

    Create g_cpp_functions_sum.cpp

---
+++ g_cpp_functions_sum.cpp
@@ -0,0 +1,7 @@
+//Given three numbers A, B, C you have to write a function named calcSum() which takes these 3 numbers as arguments and returns their sum.
+
+//solution
+int calcSum(int a, int b, int c) {
+    return a+b+c;
+    
+}
