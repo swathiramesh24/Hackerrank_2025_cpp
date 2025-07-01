2a525ba684af7964672eade6d34bbea4fe62e867
Swathi Ramesh <157286054+swathiramesh24@users.noreply.github.com>
2025-07-01 21:02:25 +0530

    Create cpp_pointers.cpp

---
+++ cpp_pointers.cpp
@@ -0,0 +1,13 @@
+/*
+Given a pointer to a variable a, the task is to complete the function updateVar() which will increment the value of the variable by 10. The function does not returns anything.
+
+Examples :
+
+Input: a = 4
+Output: 14
+*/
+
+//solution
+void updateVar(int *a) {
+   *a = (*a) + 10;
+}
