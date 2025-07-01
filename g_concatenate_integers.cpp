627eabb35c8a367a50ff9a2ea2d83ed92a20fddf
Swathi Ramesh <157286054+swathiramesh24@users.noreply.github.com>
2025-07-01 20:52:33 +0530

    Create g_concatenate_integers.cpp

---
+++ g_concatenate_integers.cpp
@@ -0,0 +1,22 @@
+/*
+Given two integers a and b, you need to concatenate them so the output is ab and print it.
+
+Examples:
+
+Input: a = 5, b = 6
+Output: 56
+Explanation: Concatenate them.
+*/
+
+//solution
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    
+    cout << a << "" << b;
+
+    return 0;
+}
