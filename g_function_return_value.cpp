46ddb1ec9747f02b7dfdfa2a54a7e8495427444a
Swathi Ramesh <157286054+swathiramesh24@users.noreply.github.com>
2025-07-01 20:50:03 +0530

    Create g_function_return_value.cpp

---
+++ g_function_return_value.cpp
@@ -0,0 +1,15 @@
+/*
+Here one integer n is given. You need to write the complete function returnValueFunction that takes n as a parameter and uses the return keyword to return double the value of n.
+
+Examples:
+
+Input: n = 2
+Output: 4
+Explanation: 2 * 2 = 4
+*/
+
+//solution
+int returnValueFunction(int n)
+{
+    return 2*n;
+}
