334d32cd65cc9830af40d8d442c51ff99ae63897
Swathi Ramesh <157286054+swathiramesh24@users.noreply.github.com>
2025-07-17 21:41:17 +0530

    Create g_type_conv.cpp

---
+++ g_type_conv.cpp
@@ -0,0 +1,23 @@
+/*
+Given a double value d, typecast it to an integer value and print it.
+
+Example:
+Input: d = 10.23
+Output: 10
+Explanation: The integer value of 10.23 is 10
+*/
+
+//code
+#include <iostream>
+using namespace std;
+
+int main() {
+    double d;
+    cin >> d;
+    
+    cout << (int)d << endl;
+
+    return 0;
+}
+
+  
